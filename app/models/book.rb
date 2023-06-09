00bca1b536c9754db82acacdcab908c00193b6a3
EC2 Default User <ec2-user@ip-172-31-7-102.ap-northeast-1.compute.internal>
2023-06-09 17:43:15 +0000

    Initial Commit1

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,11 @@
 class Book < ApplicationRecord
 
   belongs_to :user
-
+  has_many :favorites, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
   validates :title, presence: true
   validates :body, length:{ minimum: 1,maximum: 200 }, presence: true
-
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
 end
